344b3d9e6e3018e2992ff6c03d5055c09fbe1b42
saurav.sarkar <saurav.sarkar@nccbank.com.bd>
2025-04-13 18:24:10 +0600

    Java While Loop Examples

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,16 @@
 import javax.swing.*;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
+        //Java Booleans
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
 
-
-
-
-        System.out.println(" ");
-        System.out.println(" ");
-        System.out.println(" ");
+        System.out.println(isJavaFun);
+        System.out.println(isFishTasty);
 
         //Java Math
-
         int javaMath = 5;
         int javaMath1 = 10;
 
@@ -42,16 +41,7 @@ public class Main {
 
         System.out.println(randomNum);
 
-
-
         System.out.println(Math.max(javaMath,javaMath1));
-
-
-
-
-
-
-
 
         //Strings - Special Characters
 
@@ -214,6 +204,309 @@ public class Main {
 
         System.out.println("Items name "+ itemsNameOne .concat(itemsNameTwo) );
 
+
+
+
+
+        //Java If ... Else
+
+        // Java if
+        if(20>18){
+            System.out.println("20 is greater than 18");
+        }
+
+
+
+        int hiIf = 20;
+        int hiElse = 10;
+        if (hiIf > hiElse){
+            System.out.println("hiIf is greater than isElse");
+        }
+
+
+
+        int oneNumber = 5;
+        int number = 20;
+        int anotherNumber = 25;
+        if (number > oneNumber && number < anotherNumber || oneNumber == 5 ) {
+            System.out.println("Greater than 5");
+        }
+
+
+
+
+
+
+        //Java Else
+
+        int time = 20;
+        if (time < 18) {
+            System.out.println("Good day");
+        } else {
+            System.out.println("Good evening");
+        }
+
+
+        int numberIs = 5;
+        int numberAre = 10;
+
+        if (numberIs > numberAre){
+            System.out.println(numberIs + " Bigger then " + numberAre);
+        }else{
+            System.out.println(numberIs + " smaller then " + numberAre);
+        }
+
+
+
+        //Short Hand if...else
+
+        int timeIs = 20;
+        if (timeIs < 18) {
+            System.out.println("Good day.");
+        } else {
+            System.out.println("Good evening.");
+        }
+
+        int timeOne = 20;
+        String result = (timeOne < 18) ? "Good day." : "Good evening.";
+        System.out.println(result);
+
+
+
+        int numberIsShort = 5;
+        int numberAreShort = 10;
+
+        int allNumber = (numberIsShort < numberAreShort) ? numberIsShort : numberAreShort; //(condition) ? expressionTrue :  expressionFalse;
+
+        System.out.println(allNumber);
+
+
+        //Java If ... Else Examples
+
+
+        int myNum = 5;
+
+        if (myNum % 2 == 0) {
+            System.out.println(myNum + " is even");
+        } else {
+            System.out.println(myNum + " is odd");
+        }
+
+
+        int myNumber = 4;
+
+        String myNumberIs = (myNumber % 2 == 0) ? myNumber + " is even" : myNumber + " is odd";
+
+        System.out.println(myNumberIs);
+
+
+
+        //Java Switch Statements
+
+
+        int day = 3;
+
+        switch (day) {
+            case 1:
+                System.out.println("Sunday");
+                break;
+            case 2:
+                System.out.println("Monday");
+                break;
+            case 3:
+                System.out.println("Tuesday");
+                break;
+            case 4:
+                System.out.println("Wednesday");
+                break;
+            case 5:
+                System.out.println("Thursday");
+                break;
+            case 6:
+                System.out.println("Friday");
+                break;
+            case 7:
+                System.out.println("Saturday");
+                break;
+            default:
+                System.out.println("Invalid day");
+        }
+
+
+
+
+        String role = "admin";
+
+        switch (role) {
+            case "admin":
+                System.out.println("Welcome Admin!");
+                break;
+            case "user":
+                System.out.println("Welcome User!");
+                break;
+            case "guest":
+                System.out.println("Welcome Guest!");
+                break;
+            default:
+                System.out.println("Unknown role");
+        }
+
+
+
+        String device = "laptop";
+        String os = "windows";
+
+        switch (device) {
+            case "laptop":
+                switch (os) {
+                    case "windows":
+                        System.out.println("Install .exe installer");
+                        break;
+                    case "linux":
+                        System.out.println("Run shell script");
+                        break;
+                }
+                break;
+            case "mobile":
+                System.out.println("Install from app store");
+                break;
+            default:
+                System.out.println("Unknown device");
+        }
+
+
+
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println(" ");
+
+
+        int i = 0;
+        while (i < 5) {
+            System.out.println("using while loop " + i);
+            i++;
+        }
+
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println(" ");
+
+        int iWhile = 5;
+        while (iWhile > 0) {
+            System.out.println( " Using while loop "+ iWhile);
+            iWhile--;
+        }
+
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println(" ");
+
+        int iEven = 0;
+        while (iEven <= 10) {
+            if (iEven % 2 == 0) {
+                System.out.println("Even numbers using while loop " + iEven);
+            }
+            iEven++;
+        }
+
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println(" ");
+
+
+        int iSkip = 0;
+        while (iSkip < 5) {
+            iSkip++;
+            if (iSkip == 3) {
+                continue; // Skip when i is 3
+            }
+            System.out.println(iSkip);
+        }
+
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println(" ");
+
+        int doWhileI = 0;
+        do {
+            System.out.println( "Do/while Loop " + doWhileI);
+            doWhileI++;
+        } while (doWhileI < 5);
+
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println(" ");
+
+        int doWhileiOne = 1;
+        do {
+            System.out.println("i = " + doWhileiOne);
+            doWhileiOne++;
+
+        } while (doWhileiOne < 5);
+
+
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println(" ");
+
+        int loopI = 0;
+        do {
+            System.out.println(loopI);
+            loopI++;
+        }
+        while (loopI < 5);
+
+
+        //Java While Loop Examples
+
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println(" ");
+
+        int countdown = 3;
+
+        while (countdown > 0) {
+            System.out.println(countdown);
+            countdown--;
+        }
+
+        System.out.println("Happy New Year!!");
+
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println(" ");
+
+        int dice = 1;
+
+        while (dice <= 6) {
+            if (dice < 6) {
+                System.out.println("No Yatzy.");
+            } else {
+                System.out.println("Yatzy!");
+            }
+            dice = dice + 1;
+        }
+
+
+
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println(" ");
+
+
+        Random random = new Random();
+        int dice1;
+
+        while (true) {
+            dice1 = random.nextInt(10) + 1; // Random number from 1 to 6
+
+            if (dice1 == 10) {
+                System.out.println("Yatzy!");
+                break; // Stop when Yatzy (6) is rolled
+            } else {
+                System.out.println("No Yatzy.");
+            }
+        }
 
 
     }
